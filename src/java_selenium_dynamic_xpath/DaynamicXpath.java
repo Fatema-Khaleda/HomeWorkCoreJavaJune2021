e2a18947baca40b74c58268b3b0ead3c58ecceee
Fatema-Khaleda <ftmkhlda@gmail.com>
2021-08-31 19:17:06 -0400

    08312021 7:16PM

--- src/java_selenium_dynamic_xpath/DaynamicXpath.java
+++ src/java_selenium_dynamic_xpath/DaynamicXpath.java
@@ -1,38 +1,65 @@
 package java_selenium_dynamic_xpath;
 
 public class DaynamicXpath {
-	
+
 	/*
-	 
-	 Source: 
-	 
-	 1. https://www.sephora.com:
-	       - search field =   //input[@id='site_search_input;]
-	       - logo = //a[@class='css-cdhtj']
-	       
-	       //mmm
-	       //j
-	       
-	  
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
+	 * FOR DAYNAMIC XPATH PROJECT, I CHOOSE HEALTHCARE
+	 * 
+	 * 1/ UNITER HEALTHCARE 
+	 * Source: https://www.uhc.com
+	 * (i)search field : //input[@id='search-desktop'] 
+	 * (ii) submit button ://button[@id='search__button-desktop'] 
+	 * (iii) logo ://img[@alt='UnitedHealthcare'] 
+	 * (iv) logo : //a[@class='org-logo
+	 * position-relative ml-2 flex justify-center' and @target='_self']
+	 *  (v) h1 tag ://h1[contains(text(),'Contact us')]
+	 * 
+	 * 2/ Blue cross blue shield:
+	 *  Source: https://www.bcbs.com 
+	 *  (i) search field ://input[@title='Enter the terms you wish to search for.' and @id='edit-keys--2']
+	 *  (ii) submit button : //span[@class='button--inside-text'] 
+	 *  (iii) logo : //img[@alt='BCBS Logo' and @src='/themes/custom/bcbs/img/full-logo.svg'] 
+	 *  (iv)h1 tag : //h1[contains(text(),'BCBS companies support new and expectant mothers')]
+	 *  
+	 * 3/ Pfizer 
+	 * Source: https://www.pfizer.com
+	 *  (i) search field: //input[@placeholder='Search']
+	 *  (ii) submit button://button[@data-drupal-selector='edit-submit-search'] 
+	 *  (iii) logo //img[@src='https://www.pfizer.com/sites/default/files/custom-logo/Logo_Knockout.png' and @alt='Home'] 
+	 *  (iv)h1 tag ://h1[contains(@style,'opacity: 1;')]
+	 * 
+	 * 
+	 * 4/ Astrazeneca:
+	 *  Source: https://www.astrazeneca.com 
+	 *  (i) search field://input[@type='text'] 
+	 *  (ii) submit button ://span[@class='navigation__search-accessible-text'] 
+	 *  (iii) logo://img[@class='navigation__logo--color']
+	 *  (iv)h1 tag ://h1[contains(@class,'hero-header__title')]
+	 * 
+	 * 
+	 * 5/Moderna
+	 * Source: https://www.modernatx.com 
+	 * (i) search field://input[@placeholder='Search'] 
+	 * (ii) submit button : //button[@id='edit-submit-1'] 
+	 * (iii) logo : //img[@class='logo'] 
+	 * (iv)h1 tag : //h1[contains(@id,'page-title')]
+	 * 
+	 * 
+	 * 6/ Johnson & Johnson:
+	 * Source:https://www.jnj.com 
+	 * (i) search field ://input[@id='Search-input']
+	 * (ii) submit button : //button[@class='Header-search-button toggleSearch']
+	 * (iii) logo : //img[@class='Image-src' and @src='https://jnj-content-lab.brightspotcdn.com/38/1e/5acc7d074503af852c117d6c3a5f/header-logo-animation.e866e0e866d291da0a0bff6ad6860aae.gif'] 
+	 * (iv)h1 tag : //h1[contains(@class,'FullBleedLede-headline ResponsiveHeadline')]
+	 * 
+	 * 
+	 * 7/CVS 
+	 * Source:https://www.cvs.com (i) search field :
+	 * //input[@id='cvs-search-input' and @name='searchTerm'] 
+	 * (ii) logo ://a[@class='siteLogo head-sprite'] 
+	 * (iii)h1 tag ://h1[contains(@class,'css-4rbku5 css-901oao r-cqee49 r-1ui5ee8 r-vw2c0br-16krg75 r-xnfwke r-crgep1 r-t60dpp')]
+	 * 
+	 * 
 	 */
 
 }
